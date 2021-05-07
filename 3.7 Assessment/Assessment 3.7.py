f759f340e96dc0ea1fead9fbe7985d0ca959032b
ColeBramwell <Warwick2004>
2021-05-07 18:15:27 +1200

    Known bugs in sell/restock item screens due to search functionality and not setting focus upon window change. Will be redesigned regardless.

--- 3.7 Assessment/Assessment 3.7.py
+++ 3.7 Assessment/Assessment 3.7.py
@@ -28,18 +28,40 @@ class InventorySystemGUI:
         vcmd_validate_int = (root.register(lambda new_value: is_num(new_value, type_int=True)), '%P')
 
         header_frame = Frame(self.home_frame, bg=HEADER_COLOUR)
-        header_frame.grid(row=0, column=0, sticky=E + W)
+        header_frame.grid(row=0, column=0, sticky=E + W, columnspan=2)
 
         title_label = Label(header_frame, text="Inventory Tracking System", bg=HEADER_COLOUR, anchor="w")
-        title_label.grid(row=0, column=0, sticky=W, ipady=8, padx=5, ipadx=20)
-        add_item_btn = Button(header_frame, text="Add Item", command=lambda: self.activate_frame(self.add_item_frame))
-        add_item_btn.grid(row=0, column=1, sticky=E + W, padx=10)
-        sell_item_btn = Button(header_frame, text="Sell Item",
-                               command=lambda: self.activate_frame(self.sell_item_frame))
-        sell_item_btn.grid(row=0, column=2, sticky=E + W)
-        restock_item_btn = Button(header_frame, text="Restock Item",
-                                  command=lambda: self.activate_frame(self.restock_item_frame))
-        restock_item_btn.grid(row=0, column=3, sticky=E + W, padx=10)
+        title_label.grid(row=0, column=0, sticky=W, ipady=8, padx=5, ipadx=40)
+
+        search_label = Label(header_frame, text="Search by Name", bg=HEADER_COLOUR)
+        search_label.grid(row=0, column=1, sticky=E)
+        self.search_val = StringVar()
+        self.search_val.trace_add("write", lambda a, b, c: self.search())
+        self.search_entry = Entry(header_frame, textvariable=self.search_val)
+        self.search_entry.grid(row=0, column=2, sticky=W)
+        clear_search_btn = Button(header_frame, text="Clear", command=lambda: self.search_val.set(""))
+        clear_search_btn.grid(row=0, column=3, sticky=E, padx=5)
+
+        self.hidden_items = []
+
+        # Sidebar Frame (buttons)
+        self.sidebar_frame = Frame(self.home_frame)
+        self.sidebar_frame.grid(row=2, column=0, sticky="nsew")
+
+        self.sell_item_btn = Button(self.sidebar_frame, text="Sell Item",
+                                    command=lambda: self.activate_frame(self.sell_item_frame))
+        self.sell_item_btn.grid(row=0, column=0, sticky=E + W, padx=10, pady=10)
+        self.restock_item_btn = Button(self.sidebar_frame, text="Restock Item",
+                                       command=lambda: self.activate_frame(self.restock_item_frame))
+        self.restock_item_btn.grid(row=1, column=0, sticky=E + W, padx=10)
+
+        add_item_btn = Button(self.sidebar_frame, text="Add New Item",
+                              command=lambda: self.activate_frame(self.add_item_frame))
+        add_item_btn.grid(row=2, column=0, sticky=N + E + W, padx=10, pady=10)
+        self.delete_item_btn = Button(self.sidebar_frame, text="Delete Item", command=self.delete_item)
+        self.delete_item_btn.grid(row=3, column=0, sticky=N + E + W, padx=10, pady=10)
+        self.delete_all_btn = Button(self.sidebar_frame, text="Delete All", command=self.delete_all)
+        self.delete_all_btn.grid(row=4, column=0, sticky=N + E + W, padx=10)
 
         # styling the tree
         style = ttk.Style()
@@ -52,9 +74,8 @@ class InventorySystemGUI:
 
         style.map("Treeview", background=[("selected", "#123C69")])"""
 
-
         self.tree_frame = Frame(self.home_frame)
-        self.tree_frame.grid(row=2, column=0, sticky="ew")
+        self.tree_frame.grid(row=2, column=1, sticky="ew")
         self.item_tree = ttk.Treeview(self.tree_frame, selectmode='browse')
         self.item_tree.pack(side='left', fill='x', expand='1')
 
@@ -66,8 +87,8 @@ class InventorySystemGUI:
         self.item_tree["columns"] = ("1", "2", "3")
         self.item_tree['show'] = 'headings'
         self.item_tree.column("1", width=200, anchor='w')
-        self.item_tree.column("2", width=20, anchor='e', stretch=TRUE)
-        self.item_tree.column("3", width=20, anchor='e')
+        self.item_tree.column("2", width=80, anchor='e', stretch=TRUE)
+        self.item_tree.column("3", width=80, anchor='e')
         self.item_tree.heading("1", text="Name", anchor='w')
         self.item_tree.heading("2", text="Price", anchor='e')
         self.item_tree.heading("3", text="Quantity", anchor='e')
@@ -88,16 +109,17 @@ class InventorySystemGUI:
 
         # add all items to tree
         for i in range(len(self.default_items)):
-            self.item_tree.insert("", 'end', text=int(i), values=(
+            self.item_tree.insert("", 'end', values=(
                 self.default_items[i].name, "${:,.2f}".format(self.default_items[i].price),
                 self.default_items[i].quantity))
 
         # selects first row/item by default
-        first_item = self.item_tree.get_children()[0]
-        self.item_tree.focus(first_item)
-        self.item_tree.selection_set(first_item)
-        self.selected_item_iid = self.item_tree.selection()[0]
-        self.selected_item_index = self.item_tree.item(self.selected_item_iid, "text")
+        self.update_selected_item()
+
+        sell_restock_frame = Frame(self.home_frame)
+        sell_restock_frame.grid(row=3, column=0, columnspan=2, sticky="nsew")
+        sell_restock_label = Label(sell_restock_frame, text="Enter a number to sell/restock the selected item", anchor=CENTER)
+        sell_restock_label.grid(row=0, column=0, columnspan=3, sticky="ew")
 
         # ADD ITEMS FRAME
         self.add_item_frame = Frame(parent)
@@ -192,27 +214,102 @@ class InventorySystemGUI:
         restock_confirm_btn = Button(restock_button_frame, text="Confirm", command=self.restock_item)
         restock_confirm_btn.grid(row=0, column=1, padx=10)
 
-    def activate_frame(self, frame):
+    def get_selected_item_info(self):
+        info = self.item_tree.item(self.item_tree.selection()[0], 'values')
+        return [info[0], info[1], int(info[2])]
+
+    def search(self):
+        # check if any shown items should be hidden
+        for item in self.item_tree.get_children():
+            if self.search_val.get().strip().lower() not in self.item_tree.item(item, "values")[0].strip().lower():
+                self.hidden_items.append(item)
+                # deselect hidden items
+
+                if item == self.selected_item_iid:  # updates multiple times when hiding objects,
+                    # doesn't handle newly added rows yet
+                    self.item_tree.selection_remove(item)
+                    self.item_tree.detach(item)
+                    self.update_selected_item()
+                    print("updated", self.selected_item_iid)
+                else:
+                    self.item_tree.detach(item)
+        # check if any hidden items should be shown
+        new_hidden_item_list = []
+        for item in self.hidden_items:
+            if self.search_val.get().strip().lower() not in self.item_tree.item(item, "values")[0].strip().lower():
+                # hacky solution since cant delete items while iterating - stores items in a temp list and
+                # dump into main list once complete
+                new_hidden_item_list.append(item)
+            else:
+                self.item_tree.reattach(item, "", len(self.item_tree.get_children()))
+        self.hidden_items = new_hidden_item_list
+        self.update_selected_item()
+
+    def delete_item(self):
+        confirm = tkinter.messagebox.askokcancel("Delete selected item", "This action cannot be undone. Proceed?",
+                                                 icon="warning")
+        if confirm:
+            self.update_selected_item()
+            new_selected_item = self.item_tree.next(self.selected_item_iid)
+            self.item_tree.delete(self.selected_item_iid)
+            self.item_tree.focus(new_selected_item)
+            self.item_tree.selection_set(new_selected_item)
+            self.update_selected_item()
+
+    def delete_all(self):
+        confirm = tkinter.messagebox.askokcancel("Delete all items", "This action cannot be undone. Proceed?",
+                                                 icon="warning")
+        if confirm:
+            self.item_tree.delete(*self.item_tree.get_children())
+            self.update_selected_item()
+
+    def activate_frame(self, frame):  # some yucky repetitiveness here
         self.active_frame.pack_forget()
         self.active_frame = frame
         frame.pack()
 
         self.clear_all_inputs()
-
-        self.selected_item_iid = self.item_tree.selection()[0]
-        self.selected_item_index = self.item_tree.item(self.selected_item_iid, "text")
+        self.update_selected_item()
 
         if frame == self.sell_item_frame:
             self.sell_item_subtitle.configure(
-                text="Selling: " + self.item_tree.item(self.selected_item_iid, "values")[0])
+                text="Selling: " + str(self.item_tree.item(self.selected_item_iid, "values")[0]))
             self.sell_item_quantity.configure(
-                text="Quantity Remaining: " + self.item_tree.item(self.selected_item_iid, "values")[2])
+                text="Quantity Remaining: " + str(self.item_tree.item(self.selected_item_iid, "values")[2]))
 
         if frame == self.restock_item_frame:
             self.restock_item_subtitle.configure(
-                text="Selling: " + self.item_tree.item(self.selected_item_iid, "values")[0])
+                text="Restocking: " + str(self.item_tree.item(self.selected_item_iid, "values")[0]))
             self.restock_item_quantity.configure(
-                text="Quantity Remaining: " + self.item_tree.item(self.selected_item_iid, "values")[2])
+                text="Quantity Remaining: " + str(self.item_tree.item(self.selected_item_iid, "values")[2]))
+
+    def update_selected_item(self):
+        if len(self.item_tree.selection()) == 0:
+            try:
+                new_selection = self.item_tree.get_children()[0]
+                self.item_tree.focus(new_selection)
+                self.item_tree.selection_set(new_selection)
+
+                # restructure so this block only runs once
+                self.selected_item_iid = self.item_tree.get_children()[0]
+                self.sell_item_btn.configure(state=NORMAL)
+                self.restock_item_btn.configure(state=NORMAL)
+                self.delete_item_btn.configure(state=NORMAL)
+                self.delete_all_btn.configure(state=NORMAL)
+
+            except IndexError:
+                self.selected_item_iid = None
+                self.sell_item_btn.configure(state=DISABLED)
+                self.restock_item_btn.configure(state=DISABLED)
+                self.delete_item_btn.configure(state=DISABLED)
+                self.delete_all_btn.configure(state=DISABLED)
+
+        else:
+            self.selected_item_iid = self.item_tree.selection()[0]
+            self.sell_item_btn.configure(state=NORMAL)
+            self.restock_item_btn.configure(state=NORMAL)
+            self.delete_item_btn.configure(state=NORMAL)
+            self.delete_all_btn.configure(state=NORMAL)
 
     def clear_all_inputs(self):
         self.name_entry.focus()
@@ -221,6 +318,7 @@ class InventorySystemGUI:
         self.price_var.set("")
         self.quantity_var.set("")
         self.sell_quantity.set("")
+        self.restock_quantity.set("")
 
     def add_item(self):
         # displays error if any of the mandatory fields are empty
@@ -228,41 +326,41 @@ class InventorySystemGUI:
             tkinter.messagebox.showerror("Input Error",
                                          "Please ensure that all mandatory fields are completed")
         else:
-            # makes a list of all existing names in list
-            all_names = [item.name.strip().lower() for item in self.default_items]
+            # makes a list of all existing names in tree
+            all_names = [self.item_tree.item(item, 'values')[0].strip().lower() for item in
+                         self.item_tree.get_children()]
+            # check for duplicate, case insensitive
             if self.name_var.get().strip().lower() in all_names:
-                _continue = tkinter.messagebox.askyesno("Warning",
+                _continue = tkinter.messagebox.askyesno("Duplicate Entry",
                                                         "New item has same name as existing item. "
                                                         "Existing item will NOT be overwritten. Proceed?",
                                                         icon='warning')
                 if not _continue:
                     return
 
-            self.default_items.append(
-                Product(self.name_var.get(), self.description_entry.get("1.0", "end-1c"), float(self.price_var.get()),
-                        int(self.quantity_var.get())))
             self.item_tree.insert("", 'end', values=(
-                self.default_items[-1].name, "${:,.2f}".format(self.default_items[-1].price),
-                self.default_items[-1].quantity))
+                self.name_var.get(), "${:,.2f}".format(float(self.price_var.get())),
+                int(self.quantity_var.get())))
+
+            self.update_selected_item()
             self.activate_frame(self.home_frame)
 
     def sell_item(self):
         try:
-            if int(self.sell_quantity.get()) > self.default_items[self.selected_item_index].quantity:
+            current_info = self.get_selected_item_info()
+            if int(self.sell_quantity.get()) > current_info[2]:
                 tkinter.messagebox.showerror("Input Error",
                                              "Number sold cannot exceed stocked quantity")
             else:
-                self.default_items[self.selected_item_index].quantity -= int(self.sell_quantity.get())
-                self.item_tree.item(self.selected_item_iid, values=(
-                    self.default_items[self.selected_item_index].name,
-                    "${:,.2f}".format(self.default_items[self.selected_item_index].price),
-                    self.default_items[self.selected_item_index].quantity))
+                current_info[2] -= int(self.sell_quantity.get())
+                self.item_tree.set(self.item_tree.selection()[0], column=3,
+                                   value=current_info[2])
 
                 tkinter.messagebox.showinfo("Success",
                                             "Successfully sold {} units of '{}'".format(self.sell_quantity.get(),
-                                                                                        self.default_items[
-                                                                                            self.selected_item_index].name))
+                                                                                        current_info[0]))
                 self.activate_frame(self.home_frame)
+        # handles user submitting the empty string
         except ValueError:
             tkinter.messagebox.showerror("Input Error",
                                          "Please enter a number")
@@ -270,16 +368,14 @@ class InventorySystemGUI:
     # very similar to sell_item()
     def restock_item(self):
         try:
-            self.default_items[self.selected_item_index].quantity += int(self.restock_quantity.get())
-            self.item_tree.item(self.selected_item_iid, values=(
-                self.default_items[self.selected_item_index].name,
-                "${:,.2f}".format(self.default_items[self.selected_item_index].price),
-                self.default_items[self.selected_item_index].quantity))
+            current_info = self.get_selected_item_info()
+            current_info[2] += int(self.restock_quantity.get())
+            self.item_tree.set(self.item_tree.selection()[0], column=3,
+                               value=current_info[2])
 
             tkinter.messagebox.showinfo("Success",
                                         "Successfully restocked {} units of '{}'".format(self.restock_quantity.get(),
-                                                                                         self.default_items[
-                                                                                             self.selected_item_index].name))
+                                                                                         current_info[0]))
             self.activate_frame(self.home_frame)
         except ValueError:
             tkinter.messagebox.showerror("Input Error",
